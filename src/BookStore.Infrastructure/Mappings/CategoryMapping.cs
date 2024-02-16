912522cbd7b50285fde203db903b1ee45ec202bb
Seshu Babu <sbabusiliveru@agility.com>
2024-02-16 21:33:38 +0530

    Repositories added

---
+++ src/BookStore.Infrastructure/Mappings/CategoryMapping.cs
@@ -0,0 +1,26 @@
+﻿using BookStore.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStore.Infrastructure.Mappings
+{
+    public class CategoryMapping : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Name)
+                .IsRequired()
+                .HasColumnType("varchar(150)");
+
+            builder.HasMany(c => c.Books)
+                .WithOne(b => b.Category)
+                .HasForeignKey(b => b.CategoryId);
+            builder.ToTable("Categories");
+        }
+    }
+}
---
+++ src/BookStore.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using BookStore.Domain.Interfaces;
+using BookStore.Domain.Models;
+using BookStore.Infrastructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStore.Infrastructure.Repositories
+{
+    class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(BookStoreDbContext context) : base(context) { }
+    }
+}
+
